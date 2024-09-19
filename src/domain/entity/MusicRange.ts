1402a9602e3144595d921915edc65c8b7abe11f5
Hugo Potier <hugo.pot666@gmail.com>
2024-09-19 09:42:28 +0200

    Add mapper and calculate the PlanetMusic to return

---
+++ src/domain/entity/MusicRange.ts
@@ -0,0 +1,30 @@
+export const MusicRange = {
+  C: {
+    musicRange: ["C", "D", "E", "F", "G", "A", "B"],
+    interval: [0, 1.42]
+  },
+  D: {
+    musicRange: ["D", "E", "F", "G", "A", "B", "C"],
+    interval: [1.42, 2.48]
+  },
+  E: {
+    musicRange: ["E", "F", "G", "A", "B", "C", "D"],
+    interval: [2.48, 4.24]
+  },
+  F: {
+    musicRange: ["F", "G", "A", "B", "C", "D", "E"],
+    interval: [4.24, 5.66]
+  },
+  G: {
+    musicRange: ["G", "A", "B", "C", "D", "E", "F"],
+    interval: [5.66, 7.08]
+  },
+  A: {
+    musicRange: ["A", "B", "C", "D", "E", "F", "G"],
+    interval: [7.08, 8.54]
+  },
+  B: {
+    musicRange: ["B", "C", "D", "E", "F", "G", "A"],
+    interval: [8.54, 10]
+  }
+}
