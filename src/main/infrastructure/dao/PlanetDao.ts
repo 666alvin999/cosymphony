e1e6f5f28849b6ecbb6ee332f5ebde805d576be6
Alvin Hamaïde <alvin98mc@gmail.com>
2024-09-19 11:22:21 +0200

    fail (#3)

--- src/main/infrastructure/dao/PlanetDao.ts
+++ src/main/infrastructure/dao/PlanetDao.ts
@@ -3,66 +3,66 @@ import {ActionResult} from "../../domain/entity/ActionResult.js";
 
 export class PlanetDao {
 
-	public savePlanet(planet: Planet): ActionResult {
-		try {
-			planet.save();
-			return ActionResult.ActionDone();
-		} catch (e: any) {
-			return ActionResult.ActionFailed(e.message);
-		}
-	}
+  public savePlanet(planet: Planet): ActionResult {
+    try {
+      planet.save();
+      return ActionResult.ActionDone();
+    } catch (e: any) {
+      return ActionResult.ActionFailed(e.message);
+    }
+  }
 
-	public async getPlanetByName(planetName: string): Promise<Planet | ActionResult> {
-		try {
-			return await this.getPlanetByNameUsingDBOrAPI(planetName);
-		} catch(e: any) {
-			return ActionResult.ActionFailed(e.message);
-		}
-	}
+  public async getPlanetByName(planetName: string): Promise<Planet | ActionResult> {
+    try {
+      return await this.getPlanetByNameUsingDBOrAPI(planetName);
+    } catch(e: any) {
+      return ActionResult.ActionFailed(e.message);
+    }
+  }
 
-	private async getPlanetByNameUsingDBOrAPI(planetName: string): Promise<Planet | ActionResult> {
-		try {
-			const planet = await this.getPlanetByNameUsingDB(planetName);
+  private async getPlanetByNameUsingDBOrAPI(planetName: string): Promise<Planet | ActionResult> {
+    try {
+      const planet = await this.getPlanetByNameUsingDB(planetName);
 
-			if (!planet) {
-				return await this.getPlanetByNameUsingAPIAndSaveIntoDB(planetName);
-			}
+      if (!planet) {
+        return await this.getPlanetByNameUsingAPIAndSaveIntoDB(planetName);
+      }
 
-			return planet;
-		} catch(e: any) {
-			return ActionResult.ActionFailed(e.message);
-		}
-	}
+      return planet;
+    } catch(e: any) {
+      return ActionResult.ActionFailed(e.message);
+    }
+  }
 
-	private async getPlanetByNameUsingDB(planetName: string): Promise<Planet | ActionResult>  {
-		try {
-			return await Planet.findOne({id: planetName}).exec();
-		} catch(e: any) {
-			return ActionResult.ActionFailed(e.message);
-		}
-	}
+  private async getPlanetByNameUsingDB(planetName: string): Promise<Planet | ActionResult>  {
+    try {
+      return await Planet.findOne({id: planetName}).exec();
+    } catch(e: any) {
+      return ActionResult.ActionFailed(e.message);
+    }
+  }
 
-	private async getPlanetByNameUsingAPIAndSaveIntoDB(planetName: string): Promise<Planet | ActionResult> {
-		try {
-			const response = await fetch(`https://api.le-systeme-solaire.net/rest/bodies/${planetName}`);
-			const planetData = await response.json();
-			const planet: Planet = new Planet({
-				id: planetData.id,
-				name: planetData.name,
-				type: planetData.bodyType,
-				mass: {
-					value: planetData.mass.massValue,
-					exponent: planetData.mass.massExponent,
-				},
-				revolutionSpeedInDays: planetData.sideralOrbit,
-			});
+  private async getPlanetByNameUsingAPIAndSaveIntoDB(planetName: string): Promise<Planet | ActionResult> {
+    try {
+      const response = await fetch(`https://api.le-systeme-solaire.net/rest/bodies/${planetName}`);
+      const planetData = await response.json();
+      const planet: Planet = new Planet({
+        id: planetData.id,
+        name: planetData.name,
+        type: planetData.bodyType,
+        mass: {
+          value: planetData.mass.massValue,
+          exponent: planetData.mass.massExponent,
+        },
+        revolutionSpeedInDays: planetData.sideralOrbit,
+      });
 
-			this.savePlanet(planet);
+      this.savePlanet(planet);
 
-			return planet;
-		} catch (e: any) {
-			return ActionResult.ActionFailed(e.message);
-		}
+      return planet;
+    } catch (e: any) {
+      return ActionResult.ActionFailed(e.message);
+    }
 
-	}
+  }
 }
