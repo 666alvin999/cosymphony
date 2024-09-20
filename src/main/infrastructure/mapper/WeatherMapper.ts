1a2ad404fc8d087599573755ef9ddad989ed7cca
Hugo Potier <hugo.pot666@gmail.com>
2024-09-20 09:37:00 +0200

    fix: model weather

--- src/main/infrastructure/mapper/WeatherMapper.ts
+++ src/main/infrastructure/mapper/WeatherMapper.ts
@@ -1,15 +1,10 @@
-import {PlanetMusic} from "../../domain/entity/PlanetMusic.js";
-import {Planet} from "../dto/Planet.js";
-import {Synth} from "../../domain/entity/Synth.js";
-import {MusicRange} from "../../domain/entity/MusicRange.js";
 import {Weather} from "../../domain/entity/Weather";
 import {WeatherDto} from "../dto/WeatherDto";
-import {ActionResult} from "../../domain/entity/ActionResult";
 
 export class WeatherMapper {
 
-	public mapToModel(weatherDto: WeatherDto): Weather {
-		return new Weather(weatherDto.weather);
-	}
+  public mapToModel(weatherDto: WeatherDto): Weather {
+    return new Weather(weatherDto.weather);
+  }
 
 }
